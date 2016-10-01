752285bcae2152a6f045288b0a61cf8b946ad2a2
Maefdomn <resachave@gmail.com>
2016-10-02 00:25:06 +0200

    Moving stuff around

--- src/project/AdvanceWars/main/mainLauncher.java
+++ src/project/AdvanceWars/main/mainLauncher.java
@@ -3,7 +3,7 @@ package project.AdvanceWars.main;
 import project.AdvanceWars.controlleurs.MainWindowGame;
 
 public class mainLauncher {
-
+	
 	public static void main(String[] args) {
 		new MainWindowGame();
 	}
