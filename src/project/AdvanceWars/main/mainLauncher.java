73f08798fc46e2965bae271343083c71dbdc7266
Maefdomn <resachave@gmail.com>
2016-10-01 23:58:14 +0200

    Revert ""
    This reverts commit 2871c9c77665f374ba58bd799e0e12461e0af379.

---
+++ src/project/AdvanceWars/main/mainLauncher.java
@@ -0,0 +1,11 @@
+package project.AdvanceWars.main;
+
+import project.AdvanceWars.controlleurs.MainWindowGame;
+
+public class mainLauncher {
+
+	public static void main(String[] args) {
+		new MainWindowGame();
+	}
+
+}
